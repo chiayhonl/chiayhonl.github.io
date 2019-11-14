052dad36a8bd14e61e2d87e873f1dcf726ff1540
chiayhon <571617842@qq.com>
2019-11-14 08:29:13 +0800

    Site updated: 2019-11-14 08:29:09

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -63,7 +63,7 @@ function loadWidget(waifuPath, apiPath) {
 			showMessage("你都复制了些什么呀，转载要记得加上出处哦！", 6000, 9);
 		});
 		$(document).on("visibilitychange", () => {
-			if (!document.hidden) showMessage("哇，你终于回来了～", 6000, 9);
+			if (!document.hidden) showMessage("啊啦啊啦，你终于回来了呀～", 6000, 9);
 		});
 	}
 	registerEventListener();
@@ -72,13 +72,13 @@ function loadWidget(waifuPath, apiPath) {
 		var SiteIndexUrl = location.port ? `${location.protocol}//${location.hostname}:${location.port}/` : `${location.protocol}//${location.hostname}/`, text; //自动获取主页
 		if (location.href == SiteIndexUrl) { //如果是主页
 			var now = new Date().getHours();
-			if (now > 5 && now <= 7) text = "早上好！一日之计在于晨，美好的一天就要开始了。";
-			else if (now > 7 && now <= 11) text = "上午好！工作顺利嘛，不要久坐，多起来走动走动哦！";
-			else if (now > 11 && now <= 14) text = "中午了，工作了一个上午，现在是午餐时间！";
+			if (now > 6 && now <= 8) text = ["枣安枣安枣安!重要的事要对重要的人说三遍！", "要不要来杯星巴克提提神呀", "今天也要元气满满鸭！"];
+			else if (now > 8 && now <= 11) text = "上午好！工作顺利呀，要多起来走动走动哦！";
+			else if (now > 11 && now <= 14) text = "到中午啦，现在是午餐时间！为了我的糖醋里脊冲冲冲鸭！";
 			else if (now > 14 && now <= 17) text = "午后很容易犯困呢，今天的运动目标完成了吗？";
-			else if (now > 17 && now <= 19) text = "傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～";
+			else if (now > 17 && now <= 19) text = "最美不过夕阳红～！窗外夕阳的景色很美丽呢，";
 			else if (now > 19 && now <= 21) text = "晚上好，今天过得怎么样？";
-			else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，晚安～", "深夜时要爱护眼睛呀！"];
+			else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，掰掰咯～", "深夜时要爱护眼睛呀！", "别忘记和喜欢的人说晚安哦"];
 			else text = "你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？";
 		} else if (document.referrer !== "") {
 			var referrer = document.createElement("a");
@@ -99,7 +99,7 @@ function loadWidget(waifuPath, apiPath) {
 	var userAction = false,
 		hitokotoTimer = null,
 		messageTimer = null,
-		messageArray = ["好久不见，日子过得好快呢……", "大坏蛋！你都多久没理人家了呀，嘤嘤嘤～", "嗨～快来逗我玩吧！", "拿小拳拳锤你胸口！"];
+		messageArray = ["(ﾉﾟ∀ﾟ)ﾉ 好久不见鸭，日子过得好快呢……", "玲珑骰子安红豆，入骨相思知不知。" , "嘤嘤嘤~(˘•ω•˘)大坏蛋人家想你了...", "我要~~你在我身旁，我要~~你为我梳妆(*/ω＼*)", "May my missing be this poem instilling your heart."];
 	if ($(".fa-share-alt").is(":hidden")) messageArray.push("记得把小家加入 Adblock 白名单哦！");
 	$(document).mousemove(() => {
 		userAction = true;
@@ -116,18 +116,20 @@ function loadWidget(waifuPath, apiPath) {
 		}
 	}, 1000);
 
+	//显示随机消息
 	function showHitokoto() {
 		//增加 hitokoto.cn 的 API
 		if (Math.random() < 0.6 && messageArray.length > 0) showMessage(messageArray[Math.floor(Math.random() * messageArray.length)], 6000, 9);
 		else $.getJSON("https://v1.hitokoto.cn", function(result) {
-				var text = `这句一言来自 <span style="color:#0099cc;">『${result.from}』</span>，是 <span style="color:#0099cc;">${result.creator}</span> 在 hitokoto.cn 投稿的。`;
-			showMessage(result.hitokoto, 6000, 9);
-			setTimeout(() => {
-				showMessage(text, 4000, 9);
-			}, 6000);
+		    // var text = `这句一言来自 <span style="color:#0099cc;">『${result.from}』</span>，是 <span style="color:#0099cc;">${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+      showMessage(result.hitokoto, 6000, 9);
+			// setTimeout(() => {
+			// 	showMessage(text, 4000, 9);
+			// }, 6000);
 		});
 	}
 
+	//显示消息，text-消息内容, timeout-设置延时, priority-优先级
 	function showMessage(text, timeout, priority) {
 		if (!text) return;
 		if (!sessionStorage.getItem("waifu-text") || sessionStorage.getItem("waifu-text") <= priority) {
@@ -145,6 +147,7 @@ function loadWidget(waifuPath, apiPath) {
 		}
 	}
 
+	//初始化模型
 	function initModel() {
 		var modelId = localStorage.getItem("modelId"),
 			modelTexturesId = localStorage.getItem("modelTexturesId");
@@ -184,6 +187,7 @@ function loadWidget(waifuPath, apiPath) {
 	}
 	initModel();
 
+	//加载模型
 	function loadModel(modelId, modelTexturesId) {
 		localStorage.setItem("modelId", modelId);
 		if (modelTexturesId === undefined) modelTexturesId = 0;
@@ -191,6 +195,7 @@ function loadWidget(waifuPath, apiPath) {
 		loadlive2d("live2d", `${apiPath}/get/?id=${modelId}-${modelTexturesId}`, console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`));
 	}
 
+  //随机加载模型
 	function loadRandModel() {
 		var modelId = localStorage.getItem("modelId"),
 			modelTexturesId = localStorage.getItem("modelTexturesId");
@@ -207,6 +212,7 @@ function loadWidget(waifuPath, apiPath) {
 		});
 	}
 
+	//加载其他模型
 	function loadOtherModel() {
 		var modelId = localStorage.getItem("modelId");
 		$.ajax({
@@ -219,8 +225,10 @@ function loadWidget(waifuPath, apiPath) {
 			}
 		});
 	}
+
 }
 
+//初始化widget
 function initWidget(waifuPath = "/waifu-tips.json", apiPath = "") {
 	if (screen.width <= 768) return;
 	$("body").append(`<div id="waifu-toggle" style="margin-left: -100px;">
